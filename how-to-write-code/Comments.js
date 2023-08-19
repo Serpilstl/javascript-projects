ac8cd78ae3b38554cee1e92f5ff866f35d7ed49e
iyiyolsara <iyiyolsara@gmail.com>
2023-08-18 21:13:46 -0500

    Hello World

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,14 +1,4 @@
-   // This demo shows off comments!
-
-   // console.log("This does not print.");
-
+   
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
-   /* Here is how
-   to have
-   multi-line
-   comments. */
-
-   console.log("Comments make your code more readable by others.");
-   console.log("Comments make your code more readable by others.");
-
+   
